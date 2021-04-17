09d62d9179ba69c56a91622e3a63d0590aec53d1
sungu1516 <76841523+sungu1516@users.noreply.github.com>
2021-04-17 15:21:42 +0900

    java study 0407-0415

--- Java/day_0414/src/com/day/j_interface.java
+++
@@ -1,5 +0,0 @@
-package com.day;
-
-public interface j_interface {
-
-}
--- Java/day_0415/src/com/day/Gugudan_inner.java
+++
@@ -1,9 +0,0 @@
-package com.day;
-
-public class Gugudan_inner {
-	public Gugudan_inner(int n) {         // dto¿¡ ÇØ´ç
-		for (int i = 1; i < 10; i++) {
-			System.out.println(n + " * " + i + " = " + n * i);
-		}
-	}
-}
